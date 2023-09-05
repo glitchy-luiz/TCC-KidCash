fdc461ef94bfe47a9cd1542fd5d6ee1e9cfa45fc
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2023-09-05 13:11:44 -0300

    atualização frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { register } from 'swiper/element/bundle';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 register();
 
@@ -13,15 +15,23 @@ register();
 export class AppComponent {
   public appPages = [
     // title: 'Home', url: '/folder/inbox', icon: 'mail' 
-    { title: 'Home', url: '/home'},
+    { title: 'Home', url: '/folder'},
     { title: 'Metas', url: '/metas'},
     { title: 'Gastos', url: '/gastos'},
     { title: 'Deveres', url: '/deveres'},
     { title: 'Ganhos', url: '/ganhos'},
     { title: 'Estatística', url: '/estatistica'},
-    { title: 'Perfil', url: '/perfil'}
+    { title: 'Perfil', url: '/perfil'},
+    { title: 'Alterar perfil', url: '/'}
   ];
 
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private router: Router) {}
+
+  async logout() {
+    await this.authService.logout()
+    this.router.navigateByUrl('/', { replaceUrl:true});
+  }
 
 }
